78d52d600c6e48c64be0133e516fc914c7867685
richard_jli <lijin521>
2018-08-16 10:19:19 +0800

    增加router

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,14 @@ import * as React from "react";
 import "./App.css";
 import Demo from "./compoments/demoOne";
 import Pure from './compoments/pureJs';
-import logo from "./logo.svg";
-
+import Router from './router';
 class App extends React.Component {
     public render() {
         return (
             <div className="App">
                 <Demo />
                 <Pure />
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.tsx</code> and save to
-                    reload.
-                </p>
+                <Router />
             </div>
         );
     }
