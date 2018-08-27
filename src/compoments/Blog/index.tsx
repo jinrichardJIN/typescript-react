6e7a76804c9772d471dff1aa5c1dd12b0117979a
richard_jli <lijin521>
2018-08-27 15:34:32 +0800

    feat:add config js

--- src/compoments/Blog/index.tsx
+++ src/compoments/Blog/index.tsx
@@ -1,9 +1,19 @@
 import * as React from "react";
 import './index.css';
+import axios from 'axios';
 
 export default class IndexEntry extends React.Component {
     public constructor(props:any) {
         super(props);
+    }
+    public componentDidMount(){
+        axios.post('//127.0.0.1:3002/getUserInfo',{
+            userId:123
+        }).then((res)=>{
+            console.log(res);
+        }).catch((res)=>{
+            console.log(res);
+        })
     }
     public render() {
         return (
