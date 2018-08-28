079a5094971ab6c3c34b19d2f690da1062cf9ea5
richard_jli <lijin521>
2018-08-28 19:02:22 +0800

    feat:add blog list

--- src/compoments/Blog/index.tsx
+++ src/compoments/Blog/index.tsx
@@ -17,8 +17,29 @@ export default class IndexEntry extends React.Component {
     }
     public render() {
         return (
-            <div className="Entry">
-                <h1>Blog</h1>
+            <div className="Blog">
+                <p className="title">RICHARD LI</p>
+                <ul>
+                    <li>
+                        <span>AUGUST 28,2018</span>
+                        <span>vue原理的简单实现</span>
+                    </li>
+                    <li>
+                        <span>AUGUST 28,2018</span>
+                        <span>koa中间件的简单实现</span>
+                    </li>
+                    <li>
+                        <span>AUGUST 28,2018</span>
+                        <span>vue</span>
+                    </li>
+                    <li>
+                        <span>AUGUST 28,2018</span>
+                        <span>vue</span>
+                    </li>
+                </ul>
+                <pre>
+                    <code>const a = 123 </code>
+                </pre>
             </div>
         );
     }
